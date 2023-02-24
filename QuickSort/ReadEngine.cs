f20c46a54c717095ad3aa13944e1cee0b83938f5
IceStormFox <icestormfox@yahoo.com>
2023-02-24 02:00:21 +0100

    Update

---
+++ QuickSort/ReadEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace QuickSort
+{
+    internal class ReadEngine
+    {
+        public static void Reader(string path)
+        {
+            string czytnik;
+            int indeks = 0;
+            int[] array = new int[200001];
+            Stopwatch timer = new Stopwatch();
+            StreamReader dane;
+
+            dane = new StreamReader(path);
+            while ((czytnik = dane.ReadLine()) != null)
+            {
+                array[indeks] = int.Parse(czytnik);
+                indeks++;
+            }
+
+            timer.Start();
+            SortEngine.Sort(array, 0 ,array.Length);
+            timer.Stop();
+
+            Console.WriteLine("Czas wykonania sortowania: {0} ms", timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ QuickSort/SortEngine.cs
@@ -0,0 +1,34 @@
+﻿namespace QuickSort
+{
+    internal class SortEngine
+    {
+        public static void Sort(int[] array, int l, int arrayLength)
+        {
+
+            int i = l;
+            int j = arrayLength - 1;
+            int pivot = array[(l + arrayLength - 1) / 2];
+            while (i < j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int zmienna = array[i];
+                    array[i++] = array[j];
+                    array[j--] = zmienna;
+                }
+            }
+            if (l < j) Sort(array, l, j);
+            if (i < arrayLength - 1) Sort(array, i, arrayLength - 1);
+
+        }
+    }
+}
